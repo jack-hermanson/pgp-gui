ef3bb71e2c078eb6ecb3142d2ad1a1f4fa1843b8
Jack Hermanson <jack.hermanson@live.com>
2021-11-22 22:00:06 -0700

    add react front end

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+export const App: FunctionComponent = () => {
+    return (
+        <div className="App">
+            <p>dfs</p>
+        </div>
+    );
+};
