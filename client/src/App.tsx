b674d3f3732ee0a80e5b92655c379f1dd47a89f8
Jack Hermanson <jack.hermanson@live.com>
2021-11-24 17:51:29 -0700

    basic layout and react installation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,145 @@
 import { FunctionComponent } from "react";
+import "./css/main.css";
+import { Col, Container, Row } from "reactstrap";
 
 export const App: FunctionComponent = () => {
     return (
-        <div className="App">
-            <p>dfs</p>
+        <div className="body-container d-flex h-100">
+            <Container className="main-container background-blur">
+                <Row>
+                    <Col>
+                        <p>test</p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Alias architecto dignissimos dolorum eos nam.
+                            Beatae eligendi est odit omnis saepe. Ipsa ipsam
+                            laboriosam quas quasi ratione sint vitae voluptatem
+                            voluptatum!
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+            <div className="footer-container background-blur mt-auto">
+                <Container className="pb-3 pt-2 mt-2 mb-0">
+                    <Row>
+                        <Col className="text-muted d-flex">
+                            <p>Bottom footer</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 };
